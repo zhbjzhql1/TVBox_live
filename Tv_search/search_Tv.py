0b72ce200656d831f5a9a1d545da998f685ce908
zhbjzhql1 <113093533+zhbjzhql1@users.noreply.github.com>
2025-03-18 21:58:08 +0800

    Update search_Tv.py

--- Tv_search/search_Tv.py
+++ Tv_search/search_Tv.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from proxyTest import get_valid_proxies
+
 
 
 def get_url(name):
